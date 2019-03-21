6ee88c4e7c4b1621823617160d9264075afe7382
Furkan Beyhan <frknbyhn@gmail.com>
2019-03-21 08:57:48 +0300

    Initial Commit

---
+++ Stock Management/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stock Management
+//
+//  Created by Furkan Beyhan on 21.03.2019.
+//  Copyright © 2019 Furkan Beyhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
